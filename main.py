163f90d6ba67ead6bb613f2394fd573b4f0afcb1
Yuliia Ivaniuk <ivanykjula@gmail.com>
2024-08-02 18:54:41 +0300

    Add a table with data

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ filepaths = glob.glob("invoices/*.xlsx*")
 print(filepaths)
 
 for filepath in filepaths:
-    df = pandas.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
     pdf = FPDF(orientation="p", unit="mm", format="a4")
+
     pdf.add_page()
 
     filename = Path(filepath).stem
@@ -19,4 +18,39 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
     pdf.cell(w=50, h=8, txt=f"Data {invoice_date}", ln=2)
+
+    df = pandas.read_excel(filepath, sheet_name="Sheet 1")
+    names = list(df.columns)
+    modify_names = []
+
+    # Remove underline
+    for name in names:
+        print(name)
+        print(type(name))
+        new_name = name.replace("_", " ").title()
+        print(new_name)
+        modify_names.append(new_name)
+
+
+
+    print(names)
+    # Add a header
+    pdf.set_font(family="Times", size=9, style="B")
+    pdf.cell(w=30, h=8, txt=modify_names[0], border=1)
+    pdf.cell(w=70, h=8, txt=modify_names[1], border=1)
+    pdf.cell(w=30, h=8, txt=modify_names[2], border=1)
+    pdf.cell(w=30, h=8, txt=modify_names[3], border=1)
+    pdf.cell(w=30, h=8, txt=modify_names[4], border=1, ln=1)
+
+    print(df)
+
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=8, )
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
     pdf.output(f"pdfs/{filename}.pdf")
