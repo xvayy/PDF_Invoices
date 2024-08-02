04ec2cd2a665737777a9f905fce91d61cd678abf
Yuliia Ivaniuk <ivanykjula@gmail.com>
2024-08-02 16:33:45 +0300

    Generate PDF file for each invoice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
-import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx*")
 print(filepaths)
@@ -8,3 +9,11 @@ print(filepaths)
 for filepath in filepaths:
     df = pandas.read_excel(filepath, sheet_name="Sheet 1")
     print(df)
+    pdf = FPDF(orientation="p", unit="mm", format="a4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+
+    pdf.output(f"pdfs/{filename}.pdf")
